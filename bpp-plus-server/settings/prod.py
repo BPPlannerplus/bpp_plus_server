f3fbbdcb3b930a62c9619ec5a5160b222452dd9b
suy2on <tnwjdrms@naver.com>
2021-10-07 22:59:06 +0900

    fix : modify db engine

--- bpp-plus-server/settings/prod.py
+++ bpp-plus-server/settings/prod.py
@@ -5,7 +5,7 @@ ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('DATABASE_NAME'),
         'USER': env('DATABASE_USER'),
         'PASSWORD': env('DATABASE_PASSWORD'),
