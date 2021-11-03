e40489a62285be42fd08a04b7cf8b8b928d5b1ef
suy2on <tnwjdrms@naver.com>
2021-11-03 20:15:11 +0900

    # feat : 신고생성 url 추가

--- cs/urls.py
+++ cs/urls.py
@@ -5,5 +5,6 @@ app_name = 'cs'
 
 urlpatterns = [
     path('shops/<int:pk>', ReviewList.as_view()),  # 특정 shop 리뷰전체조회, 추가
-    path('<int:pk>', ReviewDetail.as_view())  # 특정 리뷰 조회, 삭제, 수정
+    path('<int:pk>', ReviewDetail.as_view()),  # 특정 리뷰 조회, 삭제, 수정
+    path('<int:pk>/complains/', ComplainList.as_view()) # 특정 리뷰에 신고추가
 ]
