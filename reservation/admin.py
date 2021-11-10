3db934e88c106b31e98246f6f15288c94a3c483f
suy2on <tnwjdrms@naver.com>
2021-11-11 02:41:28 +0900

    # feat : review 와 reservation의 admin수정

--- reservation/admin.py
+++ reservation/admin.py
@@ -4,5 +4,5 @@ from .models import *
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
-    list_display = ('id','user','shop','state','reserved_date',)
-    list_display_links = ('id','user','shop','state','reserved_date',)
+    list_display = ('id','user','shop','state','reserved_date', 'review')
+    list_display_links = ('id','user','shop','state','reserved_date', 'review')
