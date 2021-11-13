ce2dbca10b601bafcdf618c4d1e8eef2fbbcae64
suy2on <tnwjdrms@naver.com>
2021-11-14 01:54:03 +0900

    # feat : 리뷰 예약이랑 연결해서 수정

--- reservation/urls.py
+++ reservation/urls.py
@@ -6,5 +6,6 @@ app_name = 'reservation'
 urlpatterns = [
     path('', ReservationList.as_view()),  # 예약전체조회
     path('shops/<int:pk>', AddReservation.as_view()),  # shop 예약추가
+    path('<int:pk>/reviews' ReservationReviewDetail.as_view()), # 예약에 대한 리뷰 작성, 조회, 수정, 삭제
     path('<int:pk>', ReservationDetail.as_view())  # reservation 수정, 취소
 ]
