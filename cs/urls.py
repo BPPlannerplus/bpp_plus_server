ec90dca9f0d8ea54f7f82242bca44b6b4bd09a9b
suy2on <tnwjdrms@naver.com>
2021-11-14 02:31:22 +0900

    # feat : 특정 리뷰 수정 삭제

--- cs/urls.py
+++ cs/urls.py
@@ -4,7 +4,7 @@ from .views import *
 app_name = 'cs'
 
 urlpatterns = [
-    path('shops/<int:pk>', ReviewList.as_view()),  # 특정 shop 리뷰전체조회, 추가
-    path('<int:pk>', ReviewDetail.as_view()),  # 특정 리뷰 조회, 삭제, 수정
+    path('shops/<int:pk>', ReviewList.as_view()),  # 특정 shop 리뷰전체조회
+    path('<int:pk>', ReviewDetail.as_view()),
     path('<int:pk>/complains/', ComplainList.as_view()) # 특정 리뷰에 신고추가
 ]
