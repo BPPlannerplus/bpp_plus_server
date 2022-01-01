7600e042ee5b0112807ba7f9223136d3aac363ff
suy2on <tnwjdrms@naver.com>
2022-01-02 00:16:05 +0900

    # feat : 리뷰작성제한위한 cron수정

--- reservation/cron.py
+++ reservation/cron.py
@@ -6,8 +6,12 @@ def reservation_state_change():
     reservations = Reservation.objects.all()
     for reservation in reservations:
         if reservation.state == Reservation.CONFIRMED and reservation.reserved_date < datetime.date.today():
-            reservation.state = Reservation.UNREVIEWED
+            reservation.state = Reservation.UNREVIEWED # 예약만료
             reservation.save()
+        elif reservation.state == Reservation.UNREVIEWED and reservation.reserved_date + datetime.timedelta(weeks=2) < datetime.date.today():
+            reservation.state = Reservation.INVALID # 리뷰작성불가
+        elif reservation.state == Reservation.REVIEWED and reservation.reserved_date + datetime.timedelta(weeks=2) < datetime.date.today():
+            reservation.review.editable = False # 수정불가
+            
 
-def hello():
-    print("hello")
+
